2505126637275fc8c9b7f20049351fbd47e70196
nagaruikumotomi <166461273+nagaruikumotomi@users.noreply.github.com>
2024-04-23 16:08:14 +0800

    homework3.py

--- 2024_spring/lec03/homework3.py
+++ 2024_spring/lec03/homework3.py
@@ -1,25 +1,28 @@
-
-
-def cancellation(list, stop_word):
-    '''
-    Copy elements one by one from input_list into output_list. 
-    If one of the elements is equal to the stop_word, then stop the function,
-    and return what you have so far.
-    '''
-    pass
-
+def cancellation(input_list, stop_word):
+    output_list = []
+    for element in input_list:
+        if element == stop_word:
+            break
+        output_list.append(element)
+    return output_list
 def copy_all_but_skip_word(input_list, skip_word):
-    '''
-    This function should copy elements one by one from input_list into output_list.
-    If one of the elements is equal to the skip_word, then you should skip that element,
-    but keep checking all of the other elements.
-    '''
-    pass
-
+    output_list = []
+    for element in input_list:
+        if element != skip_word:
+            output_list.append(element)
+    return output_list
 def my_average(input_list):
-    '''
-    You may assume that `input_list` is a non-empty list, in which every element is a number.  
-    Calculate the average value, and return it. 
-    '''
-    pass
+    total = sum(input_list)
+    count = len(input_list)
+    return total / count
+# Testing cancellation function
+print(cancellation([3, 4, 5, 6, 7, 8], 2))  # Output: [3, 4, 5, 6, 7, 8]
+print(cancellation([3, 4, 5, 6, 7, 8], 6))  # Output: [3, 4, 5]
 
+# Testing copy_all_but_skip_word function
+print(copy_all_but_skip_word([3, 4, 5, 6, 7, 8, 3, 4, 5], 2))  # Output: [3, 4, 5, 6, 7, 8, 3, 4, 5]
+print(copy_all_but_skip_word(["a", "a", "a"], "a"))  # Output: []
+
+# Testing my_average function
+print(my_average([84, 61, 99]))  # Output: 81.33333333333333
+print(my_average([0.31147488, -0.18365408, 0.27358631, 5]))  # Output: 1.350351775289537
