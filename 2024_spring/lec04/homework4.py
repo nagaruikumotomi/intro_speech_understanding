d3d909a3325761e617cbf2fbff0c74d70ea1adf6
nagaruikumotomi <166461273+nagaruikumotomi@users.noreply.github.com>
2024-06-04 09:11:54 +0800

    Update homework4.py

--- 2024_spring/lec04/homework4.py
+++ 2024_spring/lec04/homework4.py
@@ -1,11 +1,16 @@
+import homework4, importlib
+importlib.reload(homework4)
 
 def list_to_dict(input_list):
-    '''
-    This function should return a dictionary in which each element of 
-    `input_list` is a value, and the corresponding key is the numerical 
-    index of that element in `input_list`. 
-    '''
-    pass
+    return {i: input_list[i] for i in range(len(input_list))}
+pass
+
+print(homework4.list_to_dict([1, 3.14, "hello", True]))
+print(homework4.list_to_dict(["a", "a", "a"]))
+print(homework4.list_to_dict([]))
+
+import importlib, grade
+importlib.reload(grade)
 
 
 
